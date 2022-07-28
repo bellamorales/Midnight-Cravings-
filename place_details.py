6638a11d84c3a9fa89a511478d13143472ccd3d3
LoganMercadillo <logan.mercadillo@gmail.com>
2022-07-28 13:49:03 -0500

    parsed nearby restaurants

--- place_details.py
+++ place_details.py
@@ -1,8 +1,7 @@
 import googlemaps
 import json
 from datetime import datetime
-
-
+from geo_code import get_coordinates
 
 user_location = (42.556162, -87.825082)
 place_type = "restaurant"
@@ -13,13 +12,29 @@ radius = 100
 client = googlemaps.Client(key='AIzaSyC1H9ZV4GICVuy_rY2sq4BqcQllJoWeKrU')
 
 
-nearby_restaurants = client.places(
+response = client.places(
     query="restuarant", location=user_location, radius=radius,
     region=user_region, language=user_language, open_now=True,
     type=place_type
     )
 
-print(nearby_restaurants.keys())
+nearby_restaurants = {}
+
+for restaurant in response['results']:
+    if (restaurant['business_status'] == "OPERATIONAL" 
+        and restaurant['opening_hours']['open_now']):
+        nearby_restaurants[restaurant['name']] = {
+            "place_id": restaurant["place_id"],
+            "formatted_address": restaurant["formatted_address"],
+            "types": restaurant["types"],
+            "price_level": restaurant["price_level"],
+            "rating": restaurant["rating"],
+            "open_now": restaurant['opening_hours']['open_now']
+        }
+
+assert nearby_restaurants["Uptown Restaurant"]["open_now"] == True
+
+# print(json.dumps(response['results'], indent=4))
 
 # parsed = nearby_restaurants
 
