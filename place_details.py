c690a8d66340ff407d043d68be2db45c6521572d
LoganMercadillo <logan.mercadillo@gmail.com>
2022-07-28 14:02:44 -0500

    modularized getting nearby restaurants

--- place_details.py
+++ place_details.py
@@ -3,36 +3,48 @@ import json
 from datetime import datetime
 from geo_code import get_coordinates
 
-user_location = (42.556162, -87.825082)
-place_type = "restaurant"
-user_language = "en-US"
-user_region = "US"
-radius = 100
-
 client = googlemaps.Client(key='AIzaSyC1H9ZV4GICVuy_rY2sq4BqcQllJoWeKrU')
 
 
-response = client.places(
-    query="restuarant", location=user_location, radius=radius,
-    region=user_region, language=user_language, open_now=True,
-    type=place_type
-    )
+def get_nearby_restaurants(
+    location, user_lang="en-US", user_region="US", radius=50
+    ):
+    """
+    Returns: dict of nearby restaurants as nearby_restaurants
+        Keys:
+            "place_id": string identifying restaurant in Google's Places API
+            "formatted_address": string
+            "types": list of strings identifying services at and classification of restaurant 
+            "price_level": int on scale of 0 (least expensive) to 4 (most expensive)
+            "rating": float on scale of 0-5 
+            "open_now": boolean
 
-nearby_restaurants = {}
+    """
+    response = client.places(
+        query="restuarant", location=location, radius=radius,
+        region=user_region, language=user_lang, open_now=True,
+        type="restaurant"
+        )
 
-for restaurant in response['results']:
-    if (restaurant['business_status'] == "OPERATIONAL" 
-        and restaurant['opening_hours']['open_now']):
-        nearby_restaurants[restaurant['name']] = {
-            "place_id": restaurant["place_id"],
-            "formatted_address": restaurant["formatted_address"],
-            "types": restaurant["types"],
-            "price_level": restaurant["price_level"],
-            "rating": restaurant["rating"],
-            "open_now": restaurant['opening_hours']['open_now']
-        }
+    nearby_restaurants = {}
 
-assert nearby_restaurants["Uptown Restaurant"]["open_now"] == True
+    for restaurant in response['results']:
+        if (restaurant['business_status'] == "OPERATIONAL" 
+            and restaurant['opening_hours']['open_now']):
+            nearby_restaurants[restaurant['name']] = {
+                "place_id": restaurant["place_id"],
+                "formatted_address": restaurant["formatted_address"],
+                "types": restaurant["types"],
+                "price_level": restaurant["price_level"],
+                "rating": restaurant["rating"],
+                "open_now": restaurant['opening_hours']['open_now']
+            }
+
+    return nearby_restaurants
+
+
+
+# assert nearby_restaurants["Uptown Restaurant"]["open_now"] == True
 
 # print(json.dumps(response['results'], indent=4))
 
